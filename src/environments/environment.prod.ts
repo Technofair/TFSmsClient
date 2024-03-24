59c83eccb2f28f52fa4e4006637adee3c2ff29e8
Md. Asadur Rahman <engineering1@technofairbd.com>
2024-03-24 16:40:11 +0600

    IP of MDDCN updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
 
- // baseurl: "http://localhost:5269/" //Dev: Local
+  baseurl: "http://localhost:5269/" //Dev: Local
  //baseurl: "http://69.30.205.214:8045/"//Dev: Server
  // baseurl: "http://69.30.205.214:2350/"//Prod: NV
    //baseurl: "http://69.30.205.214:2360/"//Prod: MD
-   baseurl: "http://69.30.205.214:2371/" //Prod: MDDCN
+   // baseurl: "http://192.168.0.1:2371/" //Prod: MDDCN
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
   //baseurl: "http://69.30.205.214:8045/"//Dev: Server
    //baseurl: "http://69.30.205.214:2350/"//Prod: NV
     //baseurl: "http://69.30.205.214:2360/"//Prod: MD
-    // baseurl: "http://69.30.205.214:2371/" //Prod: MDDCN
+    // baseurl: "http://192.168.0.1:2371/" //Prod: MDDCN
 };
 
 /*
