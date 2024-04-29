a9bb2a9f29129935efa78f91e9ea6fe645b74f1d
Asad <notun01@gmail.com>
2024-04-29 11:46:44 +0600

    Environment Pushed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,9 +6,10 @@ export const environment = {
   production: true,
  baseurl: "http://localhost:5269/" //Dev: Local
   //baseurl: "http://69.30.205.214:8045/"//Dev: Server
-   //baseurl: "http://69.30.205.214:2350/"//Prod: NV
-    //baseurl: "http://69.30.205.214:2360/"//Prod: MD
-    // baseurl: "http://192.168.0.1:2371/" //Prod: MDDCN
+ // baseurl: "http://103.183.116.135:2361/" //Prod: MDDCN
+   //baseurl: "http://103.183.116.14:2351/" //Prod:NV
+   //baseurl: "http://103.183.116.14:2371/" //Prod:SUN
+  //baseurl: "http://103.183.116.14:2301/" //Prod:Dev
 };
 
 /*
