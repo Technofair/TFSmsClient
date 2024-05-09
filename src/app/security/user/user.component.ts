0063da6ee262e08330cb60b8282b1cb98b2f398f
Md Shafiful Hasan Sohag <shsohag1995@gmail.com>
2024-05-09 11:38:51 +0600

    update user

--- src/app/security/user/user.component.ts
+++ src/app/security/user/user.component.ts
@@ -34,7 +34,7 @@ export class UserComponent implements OnInit {
   ngOnInit(): void {
     this.frm = new FormGroup({
       id: new FormControl(0),
-      hrmEmployeeId: new FormControl(),
+      hrmEmployeeId: new FormControl(0,Validators.required),
       loginID: new FormControl("", Validators.required),
       password: new FormControl("", Validators.required),
       isActive: new FormControl(true, Validators.required),
