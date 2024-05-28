0f489f39db5fdff473f6c14054858a3b09b58ed7
Md Shafiful Hasan Sohag <shsohag1995@gmail.com>
2024-05-28 11:18:35 +0600

    add company package .

--- src/app/technofair/technofair-routing.module.ts
+++ src/app/technofair/technofair-routing.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ClientInfoComponent } from './client-info/client-info.component';
 import { ClientServicePermissionComponent } from './client-service-permission/client-service-permission.component';
+import { ClientPackageComponent } from './client-package/client-package.component';
+import { CompanyCollectionComponent } from './company-collection/company-collection.component';
+import { CompanyPackageComponent } from './company-package/company-package.component';
 
 const routes: Routes = [
   { path: 'clientinfo', component: ClientInfoComponent },
   { path: 'client-service-permission', component: ClientServicePermissionComponent },
+  { path: 'client-package', component: ClientPackageComponent },
+  { path: 'company-package', component: CompanyPackageComponent },
+  { path: 'company-collection', component: CompanyCollectionComponent },
+  
 ];
 
 @NgModule({
